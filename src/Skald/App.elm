f6b208595a60f46037b4f1b7c2beb2ac4e9709e9
Ian D. Bollinger <ian.bollinger@gmail.com>
2015-08-19 17:50:50 -0400

    Add license headers

--- src/Skald/App.elm
+++ src/Skald/App.elm
@@ -1,3 +1,9 @@
+-- Copyright 2015 Ian D. Bollinger
+--
+-- Licensed under the MIT license <LICENSE or
+-- http://opensource.org/licenses/MIT>. This file may not be copied, modified,
+-- or distributed except according to those terms.
+
 module Skald.App
   ( run
   ) where
